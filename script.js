692fbcf0815e2c37ba777879b4c6a7fcc1414119
Ender57 <38802932+Ender57@users.noreply.github.com>
2025-08-18 13:41:53 +0300

    Update script.js

--- script.js
+++ script.js
@@ -101,6 +101,7 @@ const isAutomated = () => {
     const data = Array.from({ length: 50 }, () => Object.fromEntries(Array.from({ length: 500 }, (_, i) => [String(i), String(i)])));
     const now = Date.now();
     console.table(data);
+    console.clear();
     return Date.now() - now > 10;
 }
 
