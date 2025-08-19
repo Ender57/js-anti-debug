570cf949fa6ebaadcf242f19455792d7aa96df6d
Ender57 <38802932+Ender57@users.noreply.github.com>
2025-08-19 15:30:34 +0300

    Create tamper.js

---
+++ tamper.js
@@ -0,0 +1,60 @@
+const tamperCheck = () => {
+    const maxTouchPoints = navigator.maxTouchPoints;
+
+    if (typeof maxTouchPoints === 'number') {
+        return maxTouchPoints > 1;
+    }
+
+    const userAgent = navigator.userAgent;
+    const platform = navigator.platform;
+
+    if (platform && typeof platform === 'string') {
+        if ((!/(mac|win)/i.test(platform) && /(android|iphone|ipad|ipod|arch)/i.test(platform)) ||
+            /(iphone|ipad|ipod|ios|android)/i.test(userAgent)) {
+            return false;
+        }
+
+        if (/Windows NT/i.test(userAgent) && /Linux/i.test(platform)) {
+            return true;
+        }
+    }
+
+    const target = console && console.table;
+
+    if (!target) {
+        return true;
+    }
+
+    if (typeof target !== 'function') {
+        return true;
+    }
+
+    const hasOwnProperty = Object.prototype.hasOwnProperty;
+    const obj = {
+        o5wsdsn9rq: 1
+    };
+
+    if (!hasOwnProperty.call(obj, 'o5wsdsn9rq') || hasOwnProperty.call(obj, 'lai4yygjxq')) {
+        return true;
+    }
+
+    if (!/\[native code\]/.test(safeFunctionToString(target))) {
+        return true;
+    }
+
+    if (safeObjectToString(target) !== '[object Function]') {
+        return true;
+    }
+
+    if (target.name !== 'table') {
+        return true;
+    }
+
+    if (Object.getPrototypeOf(target) !== Function.prototype) {
+        return true;
+    }
+
+    return false;
+}
+
+alert(tamperCheck());
